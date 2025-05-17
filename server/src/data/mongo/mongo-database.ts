13e4a548224cb20139a67f7b659fa0157dd32bda
Sergio Duran <538ser@gmail.com>
2025-05-16 19:08:20 -0600

    Created the MongoDB class and connect method

---
+++ server/src/data/mongo/mongo-database.ts
@@ -0,0 +1,29 @@
+import mongoose, { mongo } from 'mongoose';
+
+interface ConnectionOptions {
+    mongoUrl: string,
+    dbName: string,
+}
+
+
+export class MongoDatabase {
+
+
+    static async connect ( options: ConnectionOptions ) {
+        const { dbName, mongoUrl } = options;
+
+
+        try {            
+            await mongoose.connect( mongoUrl, {
+                dbName: dbName,
+            } )
+
+            console.log('Mongo Database connected succesfully');
+            return true;
+
+        } catch (error) {
+            console.log({ error: error })
+            throw error;
+        }
+    }
+}
