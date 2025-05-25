16ef423f1240f958cc33ab134703fbbe66ed6006
Sergio Duran <538ser@gmail.com>
2025-05-24 18:27:22 -0600

    Change variable in user.model.ts

--- server/src/data/mongo/models/user.model.ts
+++ server/src/data/mongo/models/user.model.ts
@@ -1,21 +1,24 @@
 import mongoose from 'mongoose'
 
-export const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
 
     name: {
         type: String,
-        required: [ true, 'Name is required' ]
+        required: [ true, 'Name is required' ],
+        trim: true,
     },
 
     email: {
         type: String,
         required: [ true, 'Email is required' ],
         unique: true,
+        trim: true,
     },
 
     password: {
         type: String,
-        required: [ true, 'Password is required' ]
+        required: [ true, 'Password is required' ],
+        trim: true,
     }
 });
 
