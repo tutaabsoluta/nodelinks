b334076d2ba2c9a24d044ed0db343fcf30b13435
Sergio Duran <538ser@gmail.com>
2025-08-01 16:14:57 -0600

    Created the repository and datasource

--- server/src/domain/index.ts
+++ server/src/domain/index.ts
@@ -1,6 +1,6 @@
 
 
-export * from './dtos/login-user.dto';
-export * from './dtos/register-user.dto';
+export * from '../presentation/dtos/login-user.dto';
+export * from '../presentation/dtos/register-user.dto';
 export * from './entities/user.entity';
 export * from './errors/custom.error';
